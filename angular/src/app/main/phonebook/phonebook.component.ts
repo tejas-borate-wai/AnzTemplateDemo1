70e0b739cb83d53bb18ebd371a7b465072a853cb
Tejas Borate <tejas.borate@waiin.com>
2025-07-11 01:35:31 +0530

    change package and integrate getpeoplemethod

--- angular/src/app/main/phonebook/phonebook.component.ts
+++ angular/src/app/main/phonebook/phonebook.component.ts
@@ -1,7 +1,8 @@
 
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { PersonListDto, PersonServiceProxy } from '@shared/service-proxies/service-proxies';
 
 @Component({
     templateUrl: './phonebook.component.html',
@@ -9,10 +10,28 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
     styleUrls: ['./phonebook.component.css']
 })
 
-export class PhonebookComponent extends AppComponentBase {
+export class PhonebookComponent extends AppComponentBase implements OnInit {
+    people: PersonListDto[] = [];
+    filter: string = '';
+
     constructor(
-        injector: Injector
+        injector: Injector,
+        private _personService: PersonServiceProxy
     ) {
         super(injector);
     }
-}
+
+    ngOnInit(): void {
+        this.getPeople();
+    }
+
+    getPeople(): void {
+        this._personService.getPeople(this.filter).subscribe((result) => {
+            this.people = result.items;
+        });
+    }
+
+  }
+    
+
+
--- angular/src/shared/service-proxies/service-proxy.module.ts
+++ angular/src/shared/service-proxies/service-proxy.module.ts
@@ -7,6 +7,7 @@ import { ZeroTemplateHttpConfigurationService } from './zero-template-http-confi
 
 @NgModule({
     providers: [
+        ApiServiceProxies.PersonServiceProxy,
         ApiServiceProxies.AuditLogServiceProxy,
         ApiServiceProxies.CachingServiceProxy,
         ApiServiceProxies.ChatServiceProxy,
